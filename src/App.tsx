56a62a2a1ff06658ff240c1693371b6578a5f653
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-11-20 02:04:51 -0300

    alias testando

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-export default function App() {
-  return (
-    <>
-      <span className="text-red-600">Projeto Limpo</span>
-    </>
-  )
+import Test from "@/components/shared/test";
+
+function App() {
+	return (
+    <span className="text-red-600">
+      Projeto Limpo <Test />
+    </span>
+	);
 }
+
+export default App;
---
+++ src/components/shared/test.tsx
@@ -0,0 +1,7 @@
+import type React from "react";
+
+const Test: React.FC = () => {
+	return <div></div>;
+};
+
+export default Test;
