794db7bac7ab4a3ec59559f8f2b471481c93f219
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-11-20 02:48:18 -0300

    App.tsx impoprtando e chamando Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import Test from "@/components/shared/test";
+import { Routes } from "./routes";
 
 function App() {
 	return (
-    <span className="text-red-600">
-      Projeto Limpo <Test />
-    </span>
+    <Routes />
 	);
 }
 
