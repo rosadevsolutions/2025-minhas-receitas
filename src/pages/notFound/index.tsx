58684a2b45cf45fcd95bf8388b324133297a51ec
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-11-20 02:47:19 -0300

    route notFound  criada

---
+++ src/pages/notFound/index.tsx
@@ -0,0 +1,7 @@
+import type React from "react";
+
+const NotFound: React.FC = () => {
+	return <div>Página Não Encontrada</div>;
+};
+
+export default NotFound;
---
+++ src/routes/index.tsx
@@ -0,0 +1,36 @@
+import RootLayout from "@/components/ui/rootLayout";
+import NotFound from "@/pages/notFound";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{
+				path: "/",
+				element: <h1>Home</h1>,
+			},
+			{
+				path: "/novas-receitas",
+				element: <h1>Novas Receitas</h1>,
+			},
+			{
+				path: "/receitas-favoritas",
+				element: <h1>Receitas Favoritas</h1>,
+			},
+			{
+				path: "/receita",
+				element: <h1>Receita</h1>,
+			},
+		],
+	},
+	{
+		path: "*",
+		element: <NotFound />,
+	},
+]);
+
+export function Routes() {
+	return <RouterProvider router={router} />;
+}
