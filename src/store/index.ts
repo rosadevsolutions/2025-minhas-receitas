f3ff3b81548a98ec4c32229d79ab5669ff38aa90
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-11-25 00:00:21 -0300

    sttore configuracao

---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+//store recebe um objeto
+export const store = configureStore({
+  reducer: {},
+});
+
+// Exportar os tipos do redux.
+// Util no consumo dos dados da store.
+// RootState - recebe ReturnType (o retorno dos tipos) dos estados armazenados na store
+export type RootState = ReturnType<typeof store.getState>;
+
+// Função pra disparar ações do redux
+// Ex: É necessário disparar uma dispatch para Modificar um valor na store global
+export type AppDispatch = typeof store.dispatch;
