29303c31280d5413b5b4063ae661ad93f130c3b4
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-11-30 00:54:28 -0300

    header links navegaveis

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -1,25 +1,33 @@
 import { PathRoutes } from "@/@types/enums";
 import type React from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 const Header: React.FC = () => {
+	const navigate = useNavigate();
+	const onRedirect = () => {
+		navigate(PathRoutes.NOVAS_RECEITAS);
+	};
+
 	return (
 		<header className="flex items-center justify-center bg-white w-full h-[116px]">
 			<section className="flex items-center justify-between w-[1276px]">
 				<div className="flex items-center justify-between w-[520px]">
-					<h1 className="text-[28px] font-extrabold text-orange-600 leading-1 font-[Fredoka]">
+					<Link
+						to={PathRoutes.HOME}
+						className="text-orange-600 text-[28px] font-bold leading-1 font-[Fredoka] cursor-pointer"
+					>
 						MyReceipes
-					</h1>
+					</Link>
 					<nav className="flex items-center justify-between w-[320px]">
 						<Link
 							to={PathRoutes.RECEITAS_FAVORITAS}
-							className="text-[18px] font-normal text-gray-700 hover:text-orange-600 leading-1 font-[Fredoka] cursor-pointer"
+							className="text-gray-700 hover:text-orange-600 text-[18px] font-normal leading-1 font-[Fredoka] cursor-pointer"
 						>
 							Receitas Favoritas
 						</Link>
 						<Link
 							to={PathRoutes.RECEITA}
-							className="text-[18px] font-normal text-gray-700 hover:text-orange-600 leading-1 font-[Fredoka] cursor-pointer"
+							className="text-gray-700 hover:text-orange-600 text-[18px] font-normal  leading-1 font-[Fredoka] cursor-pointer"
 						>
 							Receitas Públicas
 						</Link>
@@ -28,6 +36,7 @@ const Header: React.FC = () => {
 				<button
 					type="button"
 					className="flex justify-center items-center w-[200px] h-[50px] rounded-[15px] cursor-pointer bg-orange-600 hover:opacity-70 transition ease-in-out duration-800 hover:scale-103"
+					onClick={onRedirect}
 				>
 					<span className="text-white text-[16px] font-[Inter] font-semibold leading-1">
 						Nova Receita
