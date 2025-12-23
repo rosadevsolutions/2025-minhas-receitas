149f74668dbdff427347e25ea843aa5c15753283
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-12-23 00:08:57 -0300

    header  ajustes finos

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -4,21 +4,20 @@ import { Link, useNavigate } from "react-router";
 
 const Header: React.FC = () => {
 	const navigate = useNavigate();
-	const onRedirect = () => {
+	const onNovasReceitas = () => {
 		navigate(PathRoutes.NOVAS_RECEITAS);
 	};
 
 	return (
 		<header className="flex items-center justify-center bg-white w-full h-[116px]">
-			<section className="flex items-center justify-between w-[1276px]">
-				<div className="flex items-center justify-between w-[520px]">
-					<Link
-						to={PathRoutes.HOME}
-						className="text-orange-600 text-[28px] font-bold leading-1 font-[Fredoka] cursor-pointer"
-					>
-						MyReceipes
+			<section className="flex items-center justify-between w-[1240px]">
+				<div className="flex items-center justify-start w-[50%] gap-14">
+					<Link to={PathRoutes.HOME} className="cursor-pointer">
+						<span className="text-orange-600 text-[28px] font-bold leading-none font-[Fredoka]">
+							Minhas Receitas
+						</span>
 					</Link>
-					<nav className="flex items-center justify-between w-[320px]">
+					<nav className="flex items-center justify-start gap-10">
 						<Link
 							to={PathRoutes.RECEITAS_FAVORITAS}
 							className="text-gray-700 hover:text-orange-600 text-[18px] font-normal leading-1 font-[Fredoka] cursor-pointer"
@@ -36,7 +35,7 @@ const Header: React.FC = () => {
 				<button
 					type="button"
 					className="flex justify-center items-center w-[200px] h-[50px] rounded-[15px] cursor-pointer bg-orange-600 hover:opacity-70 transition ease-in-out duration-800 hover:scale-103"
-					onClick={onRedirect}
+					onClick={onNovasReceitas}
 				>
 					<span className="text-white text-[16px] font-[Inter] font-semibold leading-1">
 						Nova Receita
